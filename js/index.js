6172e35f63013274e4e3049e58f0a03c8c61d64d
Spectreapple <127936650+Spectreapple@users.noreply.github.com>
2023-03-23 09:40:51 +0100

    1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
-document.getElementById("mainTitle").innerText = "Point and click adventure";
+document.getElementById("mainTitle").innerText = "point and click adventure";
 document.getElementById("mainTitle").style.textAlign = "center";
+
+
+const offsetCharacter =16;
+
+const mainCharacter = document.getElementById("mainCharacter");
+const gameWindow = document.getElementById("gameWindow");
+
+
+gameWindow.onclick = function(e) {
+
+   var Rect = gameWindow.getBoundingClientRect();
+   var x = e.clientX - Rect.left;
+   var y = e.clientY - Rect.top;
+   mainCharacter.style.left = x - offsetCharacter +"px";
+   mainCharacter.style.top = y - offsetCharacter + "px";
+    console.log(x);
+
+
+    console.log(e.target.id);
+
+    switch(e.target.id){
+        case "door1":
+            characterAudio.play(); 
+            console.log("this is not your house bozo");
+            break;
+        case "door2":
+            //sonething insert here
+            console.log("this is not your house bozo");
+            break;
+        default:
+            // do something when it doesn't have a case
+            console.log("bozo nothing is here");
+            break;
+
+    }
+}
