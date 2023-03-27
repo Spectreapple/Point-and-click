8d0a8b55c28daf9dbfe8c9e1283be787df1a33f7
Spectreapple <127936650+Spectreapple@users.noreply.github.com>
2023-03-27 11:25:42 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,50 +1,52 @@
-document.getElementById("mainTitle").innerText = "point and click adventure";
-document.getElementById("mainTitle").style.textAlign = "center";
+document.getElementById("mainTitle").innerText = "Point and click adventure";
 
-
-const offsetCharacter =16;
+const offsetCharacter = 16;
 
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
+const characterAudio = document.getElementById("characterAudio");
 const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 
 
-gameWindow.onclick = function(e) {
+gameWindow.onclick = function (e) {
 
-   var Rect = gameWindow.getBoundingClientRect();
-   var x = e.clientX - Rect.left;
-   var y = e.clientY - Rect.top;
-   mainCharacter.style.left = x - offsetCharacter +"px";
-   mainCharacter.style.top = y - offsetCharacter + "px";
-    console.log(x);
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top;  //y position within the element.
+    mainCharacter.style.left = x - offsetCharacter + "px";
+    mainCharacter.style.top = y - offsetCharacter + "px";
 
+    console.log(e.target.id);
 
-    showSpeech("does this work?");
-
-    switch(e.target.id){
+    switch (e.target.id) {
         case "door1":
-            characterAudio.play(); 
-            showSpeech("a cave really");
+            //something insert here
+            characterAudio.play();
+            showSpeech("Ain't nothin here.");
             break;
         case "door2":
-            //sonething insert here
-            showSpeech("Nothing here yet!");
+            //something insert here
+            showSpeech("The cave is empty");
+            break;
+        case "tree":
+            //something insert here
+            showSpeech("You've come to the wrong ends brother <br> Run or you finna get popped.");
             break;
         default:
             // do something when it doesn't have a case
             hideSpeech();
             break;
-
     }
 
+    //console.log(x);
 }
 
-function showSpeech(dialogue){
-    mainCharacterSpeech.innerHTML = dialogue;
-    mainCharacterSpeech.style.opacity = 100;
+function showSpeech(dialog) {
+    mainCharacterSpeech.style.opacity = 1;
+    mainCharacterSpeech.innerHTML = dialog;
 }
 
-function hideSpeech(){
+function hideSpeech() {
     mainCharacterSpeech.style.opacity = 0;
-    mainCharacterSpeech.innerHTML = "....";
+    mainCharacterSpeech.innerHTML = "...";
 }
