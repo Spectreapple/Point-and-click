f33e9b8165b1a18bf8ec7252dd053dbfc95b0ec0
Spectreapple <127936650+Spectreapple@users.noreply.github.com>
2023-03-29 12:35:37 +0200

    final

--- js/index.js
+++ js/index.js
@@ -2,51 +2,65 @@ document.getElementById("mainTitle").innerText = "Point and click adventure";
 
 const offsetCharacter = 16;
 
+const sec = 1000;
+
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
 const characterAudio = document.getElementById("characterAudio");
 const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 
+const counterSpeech = document.getElementById("counterSpeech");
+const counterAudio = document.getElementById("counterAudio");
+const counterCharacter = document.getElementById("counterCharacter");
 
 gameWindow.onclick = function (e) {
+    if (mainCharacterSpeech.style.opacity == 0 && counterSpeech.style.opacity == 0) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
 
-    var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left; //x position within the element.
-    var y = e.clientY - rect.top;  //y position within the element.
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
-
-    console.log(e.target.id);
-
-    switch (e.target.id) {
-        case "door1":
-            //something insert here
-            characterAudio.play();
-            showSpeech("Ain't nothin here.");
-            break;
-        case "door2":
-            //something insert here
-            showSpeech("The cave is empty");
-            break;
-        case "tree":
-            //something insert here
-            showSpeech("You've come to the wrong ends brother <br> Run or you finna get popped.");
-            break;
-        default:
-            // do something when it doesn't have a case
-            hideSpeech();
-            break;
+        switch (e.target.id) {
+            case "door1":
+                //something insert here
+                showSpeech(mainCharacterSpeech, characterAudio, "this is door one.<br> And it's locked dummy...");
+                break;
+            case "door2":
+                //something insert here
+                showSpeech(mainCharacterSpeech, characterAudio, "noboby is home...<br> Come back later..");
+                break;
+            case "tree":
+                //something insert here
+                showSpeech(mainCharacterSpeech, characterAudio, "Nice tree... looking good.. You come here often? nudge nudge...");
+                break;
+            case "statue":
+                //something insert here
+                showSpeech(mainCharacterSpeech, characterAudio, "Ey what's up cow");
+                setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                setTimeout(showSpeech, 4 * sec, counterSpeech, counterAudio, "did You just call me fat?");
+                setTimeout(showSpeech, 8 * sec, mainCharacterSpeech, characterAudio, "No WHAT DO YOU MEAN");
+                break;
+            default:
+                // do something when it doesn't have a case
+                hideSpeech();
+                break;
+        }
     }
-
-    //console.log(x);
 }
 
-function showSpeech(dialog) {
-    mainCharacterSpeech.style.opacity = 1;
-    mainCharacterSpeech.innerHTML = dialog;
+function showSpeech(targetBubble, targetAudio, dialogue) {
+    //trigger speech bubble and audio
+    targetBubble.style.opacity = 1;
+    targetBubble.innerHTML = dialogue;
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+    //stop after 4 seconds the dialogue bubble and audio
+    setTimeout(hideSpeech, 4 * sec, targetBubble, targetAudio);
 }
 
-function hideSpeech() {
-    mainCharacterSpeech.style.opacity = 0;
-    mainCharacterSpeech.innerHTML = "...";
+function hideSpeech(targetBubble, targetAudio) {
+    targetBubble.style.opacity = 0;
+    targetBubble.innerHTML = "...";
+    targetAudio.pause();
 }
